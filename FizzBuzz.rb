166481ef70689551f101f1c77ab7cb74eb4b2090
ayaka.kobayashi <akikakatata0207@gmail.com>
2022-09-28 04:31:53 +0000

    second commit

---
+++ FizzBuzz.rb
@@ -0,0 +1,19 @@
+
+def fizzbuzz(num)
+    if num % 3 == 0 and num % 5 == 0
+        p "FizzBuzz"
+    elsif num % 3 == 0 
+        p "Fizz"
+    elsif num % 5 == 0
+        p "Buzz" 
+    else 
+        p num 
+    end 
+end 
+
+num_max = 100
+i=1
+while i <= num_max 
+    fizzbuzz(i)
+    i = i + 1
+end 
